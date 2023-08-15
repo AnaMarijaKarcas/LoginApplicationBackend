3433cab39a52aeb384cc13819fe0236368bef9c9
AnaMarijaKarcas <anamarijakarcas00@gmail.com>
2023-08-15 08:53:05 +0200

    Added validation for login input

---
+++ Backend/Validations/LoginValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Backend.DTOs;
+using FluentValidation;
+
+namespace Backend.Validations
+{
+    public class LoginValidator : AbstractValidator<Login>
+    {
+        public LoginValidator()
+        {
+            RuleFor(login => login.UserName).NotEmpty().EmailAddress().WithMessage("Email address not valid.");
+            RuleFor(login => login.Password).NotEmpty().MinimumLength(8).WithMessage("Must be at least 8 characters long.");
+        }
+        public void ValidateAndThrow(Login login)
+        {
+            var validationResult = Validate(login);
+
+            if (!validationResult.IsValid)
+            {
+                var errors = validationResult.Errors.Select(error => error.ErrorMessage);
+                throw new ValidationException("Validation failed " + string.Join(", ", errors));
+            }
+        }
+    }
+}
