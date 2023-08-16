f13c91c46c682325c1bbc26b185cdfb21b432819
tammtry <tammtry@users.noreply.github.com>
2023-08-16 14:08:44 +0200

    Db second  model change.

--- Backend/DTO/Registration.cs
+++ Backend/DTO/Registration.cs
@@ -13,13 +13,15 @@ namespace Backend.DTO
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
         
-        public Registration(string firstName, string lastName, string email, string password)
+        public Registration(string firstName, string lastName, string userName, string email, string password)
         {
             FirstName = firstName;
             LastName = lastName;
-            UserName = email;
+            UserName = userName;
+            Email = email;
             Password = password;
 
             var validator = new RegistrationValidator();
